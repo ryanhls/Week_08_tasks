808e26bf256b53e9870178f01aa2e879043e17d9
ryanhls <166233173+ryanhls@users.noreply.github.com>
2024-09-28 02:16:51 +0800

    Update 2_About Us.py

--- pages/2_About Us.py
+++ pages/2_About Us.py
@@ -8,6 +8,8 @@ st.write("This is a Streamlit App that demonstrates how to use the OpenAI API to
 "2. Click the 'submit' button."
 "3. The app will generate the text completion based on your prompt."
 
+st.divider()
+
 with st.expander("How to use this App"):
     st.write("1. Enter your prompt in the text area.")
     st.write("2. Click the 'submit' button.")

62bec365a336eeac02fb67e293ba8b6bf5c9dcf2
ryanhls <166233173+ryanhls@users.noreply.github.com>
2024-09-28 18:44:48 +0800

    Edits on Title

--- pages/1_Chatbot.py
+++ pages/1_Chatbot.py
@@ -12,6 +12,9 @@ st.set_page_config(
 # endregion <--------- Streamlit App Configuration --------->
 
 st.title("Streamlit App")
+st.header("Streamlit App")
+st.subheader("Streamlit App")
+st.markdown("Streamlit App")
 
 # Check if the password is correct.  
 # if not check_password():  

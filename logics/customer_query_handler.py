964b1c959d82e408b55e4fe9b731e404bc3dd07d
ryanhls <166233173+ryanhls@users.noreply.github.com>
2024-09-29 22:07:01 +0800

    Update customer_query_handler.py

--- logics/customer_query_handler.py
+++ logics/customer_query_handler.py
@@ -32,7 +32,7 @@ def identify_category_and_courses(user_message):
     Decide if the query is relevant to any specific courses
     in the Python dictionary below, which each key is a `category`
     and the value is a list of `course_name`.
-    MAKE SURE you decide based on both the `category` and `course_name`.
+    MAKE SURE you decide based on both the `category` and `course_name`, they are eequally important.
 
     If there are any relevant course(s) found, output the pair(s) of a) `course_name` the relevant courses and b) the associated `category` into a
     list of dictionary object, where each item in the list is a relevant course
